4cb429553230a7cbf2f5867ca82d43b9adf712ea
srikantkjha9 <srikantkjha8@gmail.com>
2022-01-30 14:24:13 +0530

    Java File Committed

---
+++ SomeJavaCode.java
@@ -0,0 +1,12 @@
+class SomeJavaCode{
+	public static void main(String[]args)
+	{
+		int i=2, j=4;
+		System.out.println(i+" "+j);
+		i=i+j;
+		j=i-j;
+		i=i-j;
+		System.out.println(i+" "+j);
+	}
+}
+
